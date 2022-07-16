44aa75ed117e01f9d8e415717216f91b496c0fce
johnchen383 <john.bm.chen@gmail.com>
2022-07-16 20:07:07 +1200

    doc: ex3 comments

--- example-03-servlets/src/main/java/se325/example03/servlets/HelloWorldServlet.java
+++ example-03-servlets/src/main/java/se325/example03/servlets/HelloWorldServlet.java
@@ -6,16 +6,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+//uses apache tomcat to run. configured via web.xml
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
+        //MIME: e.g., application/json
         resp.setContentType("text/plain");
 
         PrintWriter out = resp.getWriter();
         out.println("Hello, world!");
-
+        //can also set status code
     }
 }
